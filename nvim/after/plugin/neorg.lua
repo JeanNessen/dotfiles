12690a9964dd9a2651725ffb02660d047c98a7eb
Jean-Luc von Nessen <jean.luc.nessen@gmail.com>
2023-09-10 09:17:54 +0200

    Updated Nvim files, removed old packer stuff.

--- nvim/after/plugin/neorg.lua
+++
@@ -1,5 +0,0 @@
-vim.opt.conceallevel = 2
-
-vim.keymap.set("n", "<leader>toc", ":Neorg toc<CR> :vertical resize 50<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>no", ":Neorg ", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>nw", ":Neorg workspace ", { noremap = true, silent = true })
--- nvim/lua/Jeanl/remaps.lua
+++ nvim/lua/Jeanl/remaps.lua
@@ -11,3 +11,25 @@ vim.keymap.set("n", "<leader>h", "<C-w>s")
 vim.keymap.set("n", "<leader>v", "<C-w>v")
 
 vim.keymap.set("n", "<leader>pv", ":NvimTreeToggle <CR>")
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
+vim.keymap.set("n", "<leader>i", vim.lsp.buf.code_action)
+
+vim.keymap.set("n", "<A-left>", "<C-o>")
+vim.keymap.set("n", "<A-right>", "<C-i>")
+
+vim.keymap.set("v", "<leader>c", ":'<,'>norm i//<CR>")
+vim.keymap.set("v", "<leader>u", ":'<,'>norm xx<CR>")
+
+vim.keymap.set("n", "<A-o>", ":Ouroboros<CR>")
